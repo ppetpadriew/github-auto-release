9550f22367286ddbb15cd5d2a5532a79c313b35a
paibamboo <gavinsitti@hotmail.com>
2019-06-08 09:52:14 +0700

    feat: auto deploy for rubber-me-web

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require('express');
 const childProcess = require('child_process');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/api', (req, res) => {
     if (req.query.secret !== process.env.RUBBER_ME_HOOK_SECRET) {
         return res.status(401).send('Mismatched signatures');
     }
 
-    const payload = JSON.parse(req.body.payload)
+    const payload = JSON.parse(req.body.payload);
     if (payload.action !== 'published' || !payload.release) {
         console.log("Skipped. Action not matched: " + payload.action);
         return res.send(200);
@@ -26,6 +26,27 @@ app.post('/api', (req, res) => {
         }
         return res.send(200);
     });
-})
+});
 
-app.listen(port, () => console.log(`Github release hook is listening: ${port}!`))
+app.post('/web', (req, res) => {
+    if (req.query.secret !== process.env.RUBBER_ME_HOOK_SECRET) {
+        return res.status(401).send('Mismatched signatures');
+    }
+
+    const payload = JSON.parse(req.body.payload);
+    if (payload.action !== 'published' || !payload.release) {
+        console.log("Skipped. Action not matched: " + payload.action);
+        return res.send(200);
+    }
+
+    childProcess.exec(process.env.RUBBER_ME_HOOK_WEB_DEPLOY_SCRIPT, function (err, stdout, stderr) {
+        console.log(stdout);
+        if (err) {
+            console.error(err);
+            return res.send(500);
+        }
+        return res.send(200);
+    });
+});
+
+app.listen(port, () => console.log(`Github release hook is listening: ${port}!`));
